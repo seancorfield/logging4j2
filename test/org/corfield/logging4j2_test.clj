9645a3cdb50531fbe49ffbf19e7930bb70cde913
Sean Corfield <sean@corfield.org>
2024-09-04 17:14:43 -0700

    add visual comparison for c.t.l
    Signed-off-by: Sean Corfield <sean@corfield.org>

--- test/org/corfield/logging4j2_test.clj
+++ test/org/corfield/logging4j2_test.clj
@@ -1,5 +1,6 @@
 (ns org.corfield.logging4j2-test
   (:require [clojure.test :refer [deftest is]]
+            [clojure.tools.logging :as ctl]
             [org.corfield.logging4j2 :as sut]))
 
 (deftest sanity-test
@@ -10,7 +11,23 @@
   (sut/with-log-tag :outer/tag
     (sut/with-log-tag :inner/tag
       (sut/info "Hello, World!")))
-  (sut/log :info (sut/as-marker :hello/world) "Hello, World!")
-  (sut/log :warn (sut/as-marker :child :parent) "Hello, World!")
-  (sut/fatal "Hello, World!")
+  (sut/with-log-uuid
+    (sut/fatal "Hello, World!"))
+  (is true))
+
+(deftest ctl-test
+  (ctl/log :info "Hello, World!")
+  (sut/with-log-tag :hello/world
+    (sut/with-log-context {:uid 1234}
+      (ctl/info "Hello, World!")))
+  (sut/with-log-tag :outer/tag
+    (sut/with-log-tag :inner/tag
+      (ctl/info "Hello, World!")))
+  (sut/with-log-uuid
+    (ctl/fatal "Hello, World!"))
+  (is true))
+
+(deftest marker-test
+  (sut/log :info (sut/as-marker :hello/world) "Hello, Marker!")
+  (sut/log :warn (sut/as-marker :child :parent) "Hello, Marker!")
   (is true))
