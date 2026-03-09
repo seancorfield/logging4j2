46bc07407ac9ec899c6b0ae5f6a8fae742daf170
Sean Corfield <sean@corfield.org>
2026-03-09 13:31:35 -0400

    Tweak license name in build.clj
    Signed-off-by: Sean Corfield <sean@corfield.org>

--- build.clj
+++ build.clj
@@ -31,7 +31,7 @@
    [:url "https://github.com/seancorfield/logging4j2"]
    [:licenses
     [:license
-     [:name "Eclipse Public License"]
+     [:name "Eclipse Public License 2.0"]
      [:url "https://www.eclipse.org/legal/epl-2.0"]]]
    [:developers
     [:developer
